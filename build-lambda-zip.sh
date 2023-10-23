41f6ddafd5c6869b905bfafb3365475cdd88c5e0
vggonzal <vggonzal@jpl.nasa.gov>
2023-10-23 10:54:18 -0700

    docker config

--- build-lambda-zip.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-rm -rf dist/lambda-package || true
-mkdir -p dist/lambda-package
-cp -r ./hydrocron-api dist/lambda-package/
-touch dist/lambda-package/hydrocron-api/__init__.py
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
-if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-    exec /home/dockeruser/.local/bin/aws-lambda-rie /usr/bin/python -m awslambdaric $1
+#!/bin/bash
+set -e
+
+if test -f "logging.ini"; then
+    echo "Applying user-provided logging.ini"
 else
-    exec /usr/bin/python -m awslambdaric $1
+    echo "Using default logging.ini included with hydrocron. This can be overridden by mounting a python logging configuration file at $(pwd)/logging.ini"
+    python - <<'END'
+import pkgutil
+logging_conf = pkgutil.get_data('hydrocron', 'conf/logging.ini').decode("utf-8")
+with open('logging.ini', 'w') as logging_ini:
+   logging_ini.write(logging_conf)
+END
 fi
+
+uvicorn hydrocron.api:app --proxy-headers --host 0.0.0.0 --port 80 --log-config logging.ini
