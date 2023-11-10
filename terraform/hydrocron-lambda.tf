38ead7f713c584d42ba4cc8bd599ec81ffc1c492
Victoria McDonald <victoria.mcdonald@jpl.nasa.gov>
2023-11-10 12:07:31 -0800

    increase load data lambda timeout

--- terraform/hydrocron-lambda.tf
+++ terraform/hydrocron-lambda.tf
@@ -86,7 +86,7 @@ resource "aws_lambda_function" "hydrocron_lambda_load_data" {
   }
   function_name = local.load_data_function_name
   role          = aws_iam_role.hydrocron-lambda-load-data-role.arn
-  timeout       = 30
+  timeout       = 120
 
   tags = var.default_tags
 }
