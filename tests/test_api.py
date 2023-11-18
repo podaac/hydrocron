0f0c51000be4b09fee04503a78e892d2e13b82be
vggonzal <vggonzal@jpl.nasa.gov>
2023-11-17 19:51:41 -0800

    new pytest framework

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,52 +1,24 @@
-# coding: utf-8
-import pytest
-
-from tests import BaseTestCase
-
-    
-@pytest.mark.usefixtures("hydrocron_dynamo_instance")
-@pytest.skip("Needs to be refactored to test the lambda handler not the flask app", allow_module_level=True)
-class TestDefaultController(BaseTestCase):
-    """DefaultController integration test stubs"""
-
-    def test_getsubset_get(self):
-        """Test case for getsubset_get
-
-        Subset by time series for a given spatial region
-        """
-        subsetpolygon_example = '{"features":[{"type":"Feature","geometry":{"coordinates":[[-95.6499095054704,50.323685647314554],[-95.3499095054704,50.323685647314554],[-95.3499095054704,50.19088502467528],[-95.6499095054704,50.19088502467528],[-95.6499095054704,50.323685647314554]],"type":"LineString"},"properties":{}}],"type":"FeatureCollection"}'
-
-        query_string = [('feature', 'Reach'),
-                        ('subsetpolygon', subsetpolygon_example),
-                        ('start_time', '2013-10-20T19:20:30+01:00'),
-                        ('end_time', '2013-10-20T19:20:30+01:00'),
-                        ('format', 'csv')]
-        response = self.client.open(
-            '/hydrocron/HydroAPI/1.0.0/timeseriesSubset',
-            method='GET',
-            query_string=query_string)
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
-
-    def test_gettimeseries_get(self):
-        """Test case for gettimeseries_get
-
-        Get Timeseries for a particular Reach, Node, or LakeID
-        """
-        query_string = [('feature', 'Reach'),
-                        ('feature_id', '73254700251'),
-                        ('format', 'csv'),
-                        ('start_time', '2022-08-04T00:00:00+00:00'),
-                        ('end_time', '2022-08-23T00:00:00+00:00')]
-        response = self.client.open(
-            '/hydrocron/HydroAPI/1.0.0/timeseries',
-            method='GET',
-            query_string=query_string)
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
+from hydrocron.utils.constants import TEST_REACH_ID_VALUE
 
 
-if __name__ == '__main__':
-    import unittest
+def test_timeseries_lambda_handler(hydrocron_api):
+    """
+    Test the lambda handler for the timeseries endpoint
+    Parameters
+    ----------
+    hydrocron_api: Fixture ensuring the database is configured for the api
+    """
+    import hydrocron.api.controllers.timeseries
+    # TODO: Implement test
+    event = {
+        "body": {
+            "feature": "Reach",
+            "reach_id": "71224100223",
+            "start_time": "2022-08-04T00:00:00+00:00",
+            "end_time": "2022-08-23T00:00:00+00:00",
+            "output": "csv",
+            "fields": "feature_id,time_str,wse"
+        }
+    }
 
-    unittest.main()
+    assert hydrocron.api.controllers.timeseries.lambda_handler(event, {})
