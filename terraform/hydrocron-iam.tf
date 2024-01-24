58c4442c7315ad7b5ebd10a8e4e0832a162d9a87
Victoria McDonald <victoria.mcdonald@jpl.nasa.gov>
2024-01-24 14:19:39 -0800

    try function arn

--- terraform/hydrocron-iam.tf
+++ terraform/hydrocron-iam.tf
@@ -53,7 +53,7 @@ data "aws_iam_policy_document" "lambda-invoke-policy" {
     ]
     resources = [
       # "arn:aws:lambda:${data.aws_region.current.id}:${local.account_id}:*",
-      aws_lambda_function.hydrocron_lambda_load_granule.invoke_arn
+      aws_lambda_function.hydrocron_lambda_load_granule.arn
       ]
   }
 
