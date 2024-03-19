7982a34f471f64dc1da329b4a80a690c497081d7
Victoria McDonald <victoria.mcdonald@jpl.nasa.gov>
2024-03-19 11:47:45 -0700

    add sns subscription

--- terraform/hydrocron-sns.tf
+++ terraform/hydrocron-sns.tf
@@ -9,3 +9,8 @@ resource "aws_sns_topic_policy" "hydrocron_sns_topic_cnm_response_policy" {
   policy = data.aws_iam_policy_document.sns-resource-policy.json
 }
 
+resource "aws_sns_topic_subscription" "hydrocron_cnm_sqs_target" {
+  topic_arn = aws_sns_topic.hydrocron_sns_topic_cnm_response.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.hydrocron_sqs_queue_granule_ingest.arn
+}
