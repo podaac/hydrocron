9a220a0945dc473ed9da49726abe312fd4f2c8ff
Victoria McDonald <victoria.mcdonald@jpl.nasa.gov>
2024-03-25 10:57:02 -0700

    change to on-demand billing for backloading

--- terraform/hydrocron-dynamo.tf
+++ terraform/hydrocron-dynamo.tf
@@ -1,7 +1,6 @@
 resource "aws_dynamodb_table" "hydrocron-swot-reach-table" {
   name           = "hydrocron-swot-reach-table"
-  read_capacity  = "30"
-  write_capacity = "30"
+  billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "reach_id"
   range_key      = "range_start_time"
   attribute {
@@ -12,18 +11,12 @@ resource "aws_dynamodb_table" "hydrocron-swot-reach-table" {
     name = "range_start_time"
     type = "S"
   }
-  lifecycle { ignore_changes = [write_capacity, read_capacity] }
-}
 
-module "reach_table_autoscaling" {
-  source     = "snowplow-devops/dynamodb-autoscaling/aws"
-  table_name = aws_dynamodb_table.hydrocron-swot-reach-table.name
 }
 
 resource "aws_dynamodb_table" "hydrocron-swot-node-table" {
   name           = "hydrocron-swot-node-table"
-  read_capacity  = "30"
-  write_capacity = "30"
+  billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "node_id"
   range_key      = "range_start_time"
   attribute {
@@ -34,9 +27,4 @@ resource "aws_dynamodb_table" "hydrocron-swot-node-table" {
     name = "range_start_time"
     type = "S"
   }
-}
-
-module "node_table_autoscaling" {
-  source     = "snowplow-devops/dynamodb-autoscaling/aws"
-  table_name = aws_dynamodb_table.hydrocron-swot-node-table.name
 }
