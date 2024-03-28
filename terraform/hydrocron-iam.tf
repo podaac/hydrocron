8176ec1902f9a111b70be3c66cd317993a24142b
Victoria McDonald <victoria.mcdonald@jpl.nasa.gov>
2024-03-28 09:06:37 -0700

    remove remaining sqs

--- terraform/hydrocron-iam.tf
+++ terraform/hydrocron-iam.tf
@@ -135,26 +135,6 @@ data "aws_iam_policy_document" "lambda_log_to_cloudwatch" {
   }
 }
 
-data "aws_iam_policy_document" "sqs-resource-policy" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type = "Service"
-      identifiers = ["sns.amazonaws.com"]
-    }
-
-    actions = ["sqs:SendMessage"]
-    resources = [aws_sqs_queue.hydrocron_sqs_queue_granule_ingest.arn]
-
-    condition {
-      test = "ArnEquals"
-      variable = "aws.SourceArn"
-      values = [aws_sns_topic.hydrocron_sns_topic_cnm_response.arn]
-    }
-  }
-}
-
 data "aws_iam_policy_document" "sns-resource-policy" {
   statement {
     effect = "Allow"
