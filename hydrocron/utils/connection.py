22a669b5dc961d362d09dc4ca63d51557d477747
Victoria McDonald <49625194+torimcd@users.noreply.github.com>
2024-04-18 13:36:40 -0700

    Feature/issue 158 - Use Lambda role for S3 connection (#159)
    * remove edl ssm parameters from s3 resource and policies
    
    * update changelog
    
    * restore some ssm policies for connections module
    
    * remove EDL ssm parameters from main.tf
    
    ---------
    
    Co-authored-by: Frank Greguska <89428916+frankinspace@users.noreply.github.com>

--- hydrocron/utils/connection.py
+++ hydrocron/utils/connection.py
@@ -3,8 +3,6 @@ Hydrocron Connection class.
 """
 
 # Standard imports
-import base64
-import json
 import os
 from types import ModuleType
 import sys
@@ -13,10 +11,6 @@ import sys
 import boto3
 from boto3.resources.base import ServiceResource
 import botocore
-import requests
-
-# Local imports
-from hydrocron.utils import constants
 
 
 class Connection(ModuleType):
@@ -68,56 +62,12 @@ class Connection(ModuleType):
         """Return S3 session resource."""
 
         if not self._s3_resource:
-            creds = self.retrieve_credentials()
-            s3_session = boto3.session.Session(
-                aws_access_key_id=creds['accessKeyId'],
-                aws_secret_access_key=creds['secretAccessKey'],
-                aws_session_token=creds['sessionToken'],
-                region_name='us-west-2')
+
+            s3_session = boto3.session.Session()
 
             self._s3_resource = s3_session.resource('s3')
 
         return self._s3_resource
-
-    @staticmethod
-    def retrieve_credentials():
-        """Makes the Oauth calls to authenticate with EDS and return a set of s3
-        same-region, read-only credntials.
-        """
-
-        login_resp = requests.get(
-            constants.S3_CREDS_ENDPOINT,
-            allow_redirects=False,
-            timeout=5
-        )
-        login_resp.raise_for_status()
-
-        auth = f"{os.environ['EARTHDATA_USERNAME']}:{os.environ['EARTHDATA_PASSWORD']}"
-        encoded_auth = base64.b64encode(auth.encode('ascii'))
-
-        auth_redirect = requests.post(
-            login_resp.headers['location'],
-            data={"credentials": encoded_auth},
-            headers={"Origin": constants.S3_CREDS_ENDPOINT},
-            allow_redirects=False,
-            timeout=5
-        )
-        auth_redirect.raise_for_status()
-
-        final = requests.get(
-            auth_redirect.headers['location'],
-            allow_redirects=False,
-            timeout=5
-        )
-
-        results = requests.get(
-            constants.S3_CREDS_ENDPOINT,
-            cookies={'accessToken': final.cookies['accessToken']},
-            timeout=5
-        )
-        results.raise_for_status()
-
-        return json.loads(results.content)
 
 
 dynamodb_resource: ServiceResource
